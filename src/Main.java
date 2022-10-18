eb6f88be50cef8bb6fbc748b77b48fb281eb4650
madahtoxa <madahtoxa@mail.ru>
2022-10-19 01:18:57 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 3;
+        switch (i) {
+            default:
+                System.out.println("3");
+            case 1:
+                System.out.println("1");
+            case 2:
+                System.out.println("2");
+        }
+    }
+}
